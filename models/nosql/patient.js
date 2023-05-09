8a3b7a39c43f61f7c7e6dfd449317c15b47d5af5
Evargas47 <Edward.vargas47@gmail.com>
2023-05-09 12:56:32 -0500

    se agrega tabla de pacientes

---
+++ models/nosql/patient.js
@@ -0,0 +1,28 @@
+const mongoose = require.apply("mongoose")
+
+const patientScheme = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+        },
+        lastname:{
+            type:String,
+        },        
+        age:{
+            type:Number,
+        },        
+        phone:{
+            type:Number,
+        },
+        identification:{
+            type:Number,
+            unique:true,
+        },
+    },
+    {
+        timestamps:true, //CreateAt, UpdateAt
+        versionKey:false
+    }
+);
+
+module.exports = mongoose.model("patients", patientScheme)
