18fcd3dd342a36504353569f71761485e31fae37
ddsal07 <danidiaz0799@gmail.com>
2023-05-09 11:47:38 -0500

    Se crea app en Node con Express

---
+++ config/mongo.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+
+const dbConnect = () => {
+
+}
+
+module.exports = dbConnect
+
+//mongodb+srv://DaniDiaz:WC2YBymLmsKp23UC@cluster0.knzyqlk.mongodb.net/dbTest?retryWrites=true&w=majority
