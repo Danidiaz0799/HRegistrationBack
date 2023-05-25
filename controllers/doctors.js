ce9415e39debfe01d412459e3931868cf10ffdb3
ddsal07 <danidiaz0799@gmail.com>
2023-05-24 22:31:58 -0500

    Creación de put y delete de los 3 servicios

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -1,13 +1,13 @@
-const { doctorsModels } = require('../models')
+const { doctorsModels } = require('../models');
 
 /**
- *Obtener lista de doctores
+ * Obtener lista de doctores
  * @param {*} req
  * @param {*} res
  */
 const getDoctors = async (req, res) => {
-    const data = await doctorsModels.find({});
-    res.send(data)
+  const data = await doctorsModels.find({});
+  res.send(data);
 };
 
 /**
@@ -16,33 +16,72 @@ const getDoctors = async (req, res) => {
  * @param {*} res
  */
 const getDoctorsBySpecialty = async (req, res) => {
-    const { specialty } = req.params;
-    try {
-      const doctors = await doctorsModels.find({ specialties: { $in: [specialty] } });
-      if (doctors.length > 0) {
-        res.status(200).json({ data: doctors });
-      } else {
-        res.status(404).json({ message: 'No doctors found for the specified specialty' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: 'Error retrieving doctors' });
+  const { specialty } = req.params;
+  try {
+    const doctors = await doctorsModels.find({ specialties: { $in: [specialty] } });
+    if (doctors.length > 0) {
+      res.status(200).json(doctors);
+    } else {
+      res.status(404).json({ message: 'No doctors found for the specified specialty' });
     }
-  };
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving doctors' });
+  }
+};
 
 /**
- *Insertar doctor
+ * Crear un doctor
  * @param {*} req
  * @param {*} res
  */
 const postDoctor = async (req, res) => {
-    const { body } = req;
-    console.log(body);
-    const data = await doctorsModels.create(body)
-    if (data) {
-        res.status(201).json({ message: 'Doctor created successfully', data });
-    } else {
-        res.status(500).json({ message: 'Error creating doctor' });
-    }
+  const { body } = req;
+  console.log(body);
+  const data = await doctorsModels.create(body);
+  if (data) {
+    res.status(201).json({ message: 'Doctor created successfully', data });
+  } else {
+    res.status(500).json({ message: 'Error creating doctor' });
+  }
 };
 
-module.exports = { getDoctors, getDoctorsBySpecialty, postDoctor };
+/**
+ * Actualizar doctor
+ * @param {*} req
+ * @param {*} res
+ */
+const putDoctor = async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  try {
+    const doctor = await doctorsModels.findByIdAndUpdate(id, body, { new: true });
+    if (doctor) {
+      res.status(200).json({ message: 'Doctor updated successfully', data: doctor });
+    } else {
+      res.status(404).json({ message: 'Doctor not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating doctor' });
+  }
+};
+
+/**
+ * Eliminar doctor
+ * @param {*} req
+ * @param {*} res
+ */
+const deleteDoctor = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const doctor = await doctorsModels.findByIdAndDelete(id);
+    if (doctor) {
+      res.status(200).json({ message: 'Doctor deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Doctor not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting doctor' });
+  }
+};
+
+module.exports = { getDoctors, getDoctorsBySpecialty, postDoctor, putDoctor, deleteDoctor };
--- routes/doctors.js
+++ routes/doctors.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getDoctors, getDoctorsBySpecialty, postDoctor } = require("../controllers/doctors");
+const { getDoctors, getDoctorsBySpecialty, postDoctor, putDoctor, deleteDoctor } = require("../controllers/doctors");
 const router = express.Router();
 
 // http://localhost/doctors GET, POST, PUT, DELETE
 router.get("/", getDoctors);
-router.get("/:specialty", getDoctorsBySpecialty); 
+router.get("/:specialty", getDoctorsBySpecialty);
 router.post("/", postDoctor);
+router.put("/:id", putDoctor);
+router.delete("/:id", deleteDoctor);
 
 module.exports = router;
-
